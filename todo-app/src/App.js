74c54174a5ac1309651ac3d75588242d7af72630
Benahin Kur <benahink@gmail.com>
2019-07-09 17:11:22 -0600

    first commit

--- todo-app/src/App.js
+++ todo-app/src/App.js
@@ -1,23 +1,85 @@
 import React, { Component } from 'react';
 import TodoInput from './components/TodoInput.js';
 import TodoList from './components/TodoList.js';
-import 'bootstrap'
+import 'bootstrap/dist/css/bootstrap.min.css';
+import uuid from 'uuid';
 
 class App extends Component {
-    constructor(props) {
-        super(props);
-        this.state = {  }
+    state = {
+        items: [], 
+        id: uuid(),
+        item: "", 
+        editItem: false
     }
 
+    handleChange = (e) => {
+        this.setState({
+            item: e.target.value
+        })
+    }
+
+    handleSubmit = (e) => {
+        e.preventDefault();
+        const newItem = {
+            id: this.state.id, 
+            title: this.state.item
+        }
+
+        const updatedItems = [...this.state.items, newItem]
+
+        this.setState({
+            items: updatedItems, 
+            item: '', 
+            id: uuid(), 
+            editItem: false
+        })
+    }
+
+    clearList = () => {
+        this.setState({
+            items: []
+        })
+    }
+
+    handleDelete = (id) => {
+        const items = this.state.items;
+        const filteredItems = items.filter(item => item.id !== id)
+        this.setState({
+            items: filteredItems
+        })
+    }
+
+    handleEdit = (id) => {
+        const items = this.state.items;
+        const filteredItems = items.filter(item => item.id !== id)
+        
+        const selectedItem = items.find(item => item.id === id)
+        
+        console.log(selectedItem)
+        this.setState({
+            items: filteredItems, 
+            item: selectedItem.title, 
+            editItem: true, 
+            id: id
+        })
+    }
 
     render() { 
         return ( 
-        <div>
-            <h1>
-                Let's get it started!!  
-            </h1> 
-            <TodoInput />
-            <TodoList />
+        <div className="container">
+            <div className="row">
+                <div className="col-10 mx-auto col-md-8 ml-4">
+                    <h3 className="text-capitalize text-center">todo input</h3>
+                    <TodoInput  item={this.state.item} 
+                                handleChange={this.handleChange}
+                                handleSubmit={this.handleSubmit}
+                                editItem={this.state.editItem}/>
+                    <TodoList   items={this.state.items}
+                                clearList={this.clearList}
+                                handleDelete={this.handleDelete}
+                                handleEdit={this.handleEdit}/>
+                </div>
+            </div>
         </div>
         );
     }
--- todo-app/src/components/TodoList.js
+++ todo-app/src/components/TodoList.js
@@ -2,19 +2,24 @@ import React, { Component } from 'react';
 import TodoItem from './TodoItem.js';
 
 class TodoList extends Component {
-    constructor(props) {
-        super(props);
-        this.state = {  }
-    }
     render() { 
+        const { items, clearList, handleDelete, handleEdit } = this.props
         return ( 
-            <div>
-                <h1>
-                    TodoList
-                </h1>
-                <TodoItem />
-            </div>
-         );
+            <ul className="list-group my-5">
+                <h3 className="text-capitalize text-center">todo list</h3>
+                {items.map(item=>{
+                    return(
+                        <TodoItem   key={item.id} 
+                                    title={item.title}
+                                    handleDelete={() => handleDelete(item.id)}
+                                    handleEdit={() => handleEdit(item.id)}/>
+                    )
+                })}
+                <button type="button" 
+                        className="btn btn-danger btn-block text-capitalize mt-5" 
+                        onClick={clearList}>clear list</button>
+            </ul>
+        );
     }
 }
  
